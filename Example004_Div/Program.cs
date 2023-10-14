cbc02ab1d522d720a4bfb780401d5da3e8823b65
Silentlay <>
2023-10-14 17:05:12 +0300

    add info Example004_Div/Program.cs

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать часное 2х чисел
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// добавим изменения, что будет если 12 разделить на 5?
+
+double numberA = 12; // double - позволяет работать с вещественными числами
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
