65d4f06bf4df79adb84f226334bc0078bce6569c
Silentlay <>
2023-10-14 16:24:55 +0300

    add  Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Введите ваше имя"); // Console.WriteLine("текст") - команда которая выводит текст в наш терминал и переводит на новую строчку
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя "); // Console.Write("текст") - команда, которая выводит текст в наш терминал в одну строку
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
