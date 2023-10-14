af0e3314b1e3dd98a422fa79e19da0c3b4e6839f
Silentlay <>
2023-10-14 17:36:52 +0300

    add Example005_ConditionElse

---
+++ Example005_ConditionElse/Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if(username == "Маша")
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username);
+// }
+
+// // в чем тут может скрываться проблема? Если Маша будет вводить свое имя например, так: мАша, то ее программа будет приветствовать как обычного пользлвателя
+// // исправим это
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")// мы починили большую часть проблем
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
