8251c19d505d19e3e73c642866c4dfd380f04a2a
Silentlay <>
2023-10-14 17:18:19 +0300

    add pandom

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,9 +1,26 @@
-﻿// int numberA = 31; // int - целое число
+﻿// 1
+// int numberA = 31; // int - целое число
 // int numberB = 5;
 // int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
 // Console.WriteLine(numberA + numberB);
 
-int numberA = 31; // int - целое число
-int numberB = 5;
+// int numberA = 31; // int - целое число
+// int numberB = 5;
+// int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
+// Console.WriteLine(result);
+
+// //Генератор случайных чисел
+// //2
+// int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+// int numberB = new Random().Next(1,10);
+// int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
 Console.WriteLine(result);
+
+// ожидаем увидеть тройку чисел
