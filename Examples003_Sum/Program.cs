6b68d302148e61ced56bd09d722604feb69a2909
Silentlay <>
2023-10-14 16:48:13 +0300

    add Examples003_Sum

---
+++ Examples003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// int numberA = 31; // int - целое число
+// int numberB = 5;
+// int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
+// Console.WriteLine(numberA + numberB);
+
+int numberA = 31; // int - целое число
+int numberB = 5;
+int result = numberA + numberB; // result - дополнительная переменная (дополнительный контейнер) куда записываем в данном случае сумму
+Console.WriteLine(result);
